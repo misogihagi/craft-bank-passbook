8dff6b569cd2ddb389218508f53b7076255fc42d
misogihagi <47350059+misogihagi@users.noreply.github.com>
2025-07-21 19:32:28 +0900

    convert admin router

--- apps/admin/src/lib/trpc.ts
+++ apps/admin/src/lib/trpc.ts
@@ -1,5 +1,5 @@
 import { createTRPCContext } from "@trpc/tanstack-react-query";
-import type { AppRouter } from "@repo/trpc";
+import type { AdminAppRouter } from "@repo/trpc";
 export const { TRPCProvider, useTRPC, useTRPCClient } =
-  createTRPCContext<AppRouter>();
-export type { AppRouter };
+  createTRPCContext<AdminAppRouter>();
+export type { AdminAppRouter as AppRouter };
